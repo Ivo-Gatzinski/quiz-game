eda10fb7f1ea03e828b9c0bbd7076e688e05abd8
Ezhko <ucsd.camp.21@gmail.com>
2021-09-25 20:39:20 -0400

    add first two pages pseudo-code

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// EVENT ON LOAD PAGE: Display start page:
+// - display high scores link in top left corner
+// - display timer in top right corner, showing 0 seconds
+// - display welcome title
+// - display welcome message
+// - display start button
+
+
+//EVENT ON CLICK START BUTTON: 
+//
+// - Display one question:
+//
+// - Display question prompt in h2
+// 	- DISPLAY QUESTION: example of a quiz question: 
+// "What keyword can be used to print a type of a value?"
+// 
+//- show multiple choice answers:
+// - Display list of four (4) buttons with answers on labels
+// 	- display buttons with the following text below the question:
+// 	- A: true 
+// 	- B: string
+// 	- C: typeof
+// 	- D: function
+// 
+//  - timer countdown starts:
+// 	- display "Time: 120" to begin
+// 	- update the time display each second counting down
+//
+// - do NOT display Title, Welcome, Start-Button
